dbf06e4d568183730079cf717d13982bd6d23bb9
Patrick Corrigan <patrickcorrigan@Patricks-MacBook-Pro.local>
2015-08-18 11:53:55 -0700

    fix small bug in eta and print in whole minutes

--- Algorithm/Globals.swift
+++ Algorithm/Globals.swift
@@ -1,9 +1,16 @@
 typealias Meters = Double
 typealias Miles = Double
 typealias Seconds = Double
+typealias Minutes = Double
 
 extension Miles {
     func inMeters() -> Meters {
         return self * 1609.34
+    }
+}
+
+extension Seconds {
+    func inMinutes() -> Minutes {
+        return self / 60.0
     }
 }
